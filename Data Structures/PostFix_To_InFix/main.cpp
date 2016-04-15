681a611fda1b7ff65f26da336f57f6b7363618a8
Eslam-Samir <eslamsamir232@gmail.com>
2016-04-15 22:30:10 +0200

    modification

--- Data Structures/PostFix_To_InFix/main.cpp
+++ Data Structures/PostFix_To_InFix/main.cpp
@@ -8,17 +8,18 @@ void main()
 {
 	Stack<string> s;
 	string postFx;
-	string inFix, first, second;
+	string inFix, first, second, specialOp;
 	char previousOperator = ' ';
 
-	// use space as a separator (e.g.   24 5 + 4 * 76 /   ===>   (24 + 5) * 4 / 76)
+	cout << "Enter The Postfix: " << endl;
+	// use space as a separator (	e.g.	24 5 + 4 * 76 /		===>	(24 + 5) * 4 / 76	)
 	getline(cin, postFx);
 
 	for(int i = 0, n = postFx.length(); i < n; i++)
 	{
 		if(postFx.at(i) == ' ')
 			continue;
-		if(postFx.at(i) == '*' || postFx.at(i) == '/' || postFx.at(i) == '+' || postFx.at(i) == '-')
+		if(postFx.at(i) == '*' || postFx.at(i) == '/' || postFx.at(i) == '+' || postFx.at(i) == '-' || postFx.at(i) == '^')
 		{
 			second = s.pop();
 			first = s.pop();
@@ -26,9 +27,21 @@ void main()
 			{
 				first = "(" + first + ")";
 			}
+			else if((previousOperator == '+' || previousOperator == '-') && postFx.at(i) == '-')
+			{
+				second = "(" + second + ")";
+			}
 			inFix = first + " " + postFx.at(i) + " " + second;
 			s.push(inFix);
 			previousOperator = postFx.at(i);
+		}
+		else if(( specialOp = postFx.substr(i,3)) == "sin" || ( specialOp = postFx.substr(i,3)) == "cos"
+			|| ( specialOp = postFx.substr(i,3)) == "tan" || ( specialOp = postFx.substr(i,4)) == "sqrt" )
+		{
+			second = s.pop();
+			second = specialOp+"("+second+")";
+			s.push(second);
+			i += specialOp.length() - 1;
 		}
 		else
 		{
@@ -38,8 +51,14 @@ void main()
 		}
 
 	}
-	while(!s.isEmpty())
-		cout << s.pop() << endl;
-		
+	inFix = s.pop();
+	if(s.isEmpty())
+	{
+		cout << "The Infix: " << inFix << endl;
+	}	
+	else
+	{
+		cout << "Wrong Format";
+	}
 	system("pause");
 }
